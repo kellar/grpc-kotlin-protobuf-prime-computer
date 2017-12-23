f8c3e2691f2f5a6620255e76055247f3e41d361a
kellar <blakekellar@gmail.com>
2017-12-23 14:08:55 -0600

    Kotlin application

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+./gradlew clean build
+java -jar build/libs/grpc-kotlin-protobuf-0.0.1-SNAPSHOT.jar 
+```
---
+++ src/main/kotlin/com/blakekellar/grpcprimes/Application.kt
@@ -0,0 +1,5 @@
+package com.blakekellar.grpcprimes
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin world")
+}
